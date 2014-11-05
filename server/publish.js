e6754be082bf6c3c746e7c74e386d30c09f36108
Courtney Noh <courtneynoh@gmail.com>
2014-11-05 00:52:56 -0800

    Yo, fixed the buggy sharing

--- server/publish.js
+++ server/publish.js
@@ -1,10 +1,14 @@
 Meteor.publish('publicLists', function() {
-  return Lists.find({userId: {$exists: false}});
+	console.log("hi");
+	console.log(Lists.find({},{ fields: {Privacy: false}}));
+  return Lists.find({Privacy: false});
 });
 
 Meteor.publish('privateLists', function() {
   if (this.userId) {
-    return Lists.find({userId: this.userId});
+	console.log("We are in private lists");
+	console.log(Lists.find({access: {$exists: Meteor.user().emails[0].address}}));
+    return Lists.find({access: {$exists: Meteor.user().emails[0].address}});
   } else {
     this.ready();
   }
